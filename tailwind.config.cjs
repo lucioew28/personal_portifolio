1db29f8e3a50c61b8276be50671c0590868283fb
lucioew28 <lucioew28@gmail.com>
2023-03-11 17:10:00 -0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, js, ts, vue}", 
+    "./src/**/*",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+    }
+  },
+  plugins: [],
+}
